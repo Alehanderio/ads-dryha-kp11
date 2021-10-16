bf96f514f5fd64a57dba6d75dbf0f3022f87113f
Alehanderio <72273009+Alehanderio@users.noreply.github.com>
2021-10-16 11:24:48 +0300

    Лабараторна завнтажена на GitHub.
    Лабараторна завнтажена на GitHub.

---
+++ Lab_01/asd_lab_1_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Math;
+
+namespace asd_lab_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, x, y, z;
+            Console.Write("Enter x = "); x = Double.Parse(Console.ReadLine());
+            Console.Write("Enter y = "); y = Double.Parse(Console.ReadLine());
+            Console.Write("Enter z = "); z = Double.Parse(Console.ReadLine());
+            double a1 = Pow(x, z) * y - Cbrt(Pow(x, 2) - y * Pow(z, 3));
+            if (a1 == 0 || (x < 0 && 1 / z % 2 == 0))
+            {
+                Console.WriteLine("a and b are not possible to count.");
+            }
+            else
+            {
+                a = (x + y - z) / a1;
+                Console.WriteLine($"a equals {a}");
+                if (a == 0)
+                {
+                    Console.WriteLine("b is not possibl to count");
+                }
+                else
+                {
+                    b = Cos(x * y + Pow(y, 2) / Pow(a, 2));
+                    Console.WriteLine($"b equals {b}");
+                }
+            }
+        }
+    }
+}
---
+++ Lab_01/asd_lab_1_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using static System.Math;
+
+namespace asd_lab_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long temp = 0;
+            Console.WriteLine("2 digits");
+            for(int n = 10; n <= 99; n++)
+            {
+                temp = Convert.ToInt32(Pow(n / 10, 2) + Pow(n % 10, 2));
+                if(temp == n)
+                {
+                    Console.WriteLine(n);
+                }
+            }
+            Console.WriteLine("3 digits");
+            for (int n = 100; n <= 999; n++)
+            {
+                int x1 = n / 100; int x2 = n - 100 * x1;
+                temp = Convert.ToInt32(Pow(x1, 3) + Pow(x2 / 10, 3)
+                    + Pow(x2 % 10, 3));
+                if (temp == n)
+                {
+                    Console.WriteLine(n);
+                }
+            }
+            Console.WriteLine("4 digits");
+            for (int n = 1000; n <= 9999; n++)
+            {
+                int x1 = n / 1000;
+                int x2 = (n - 1000 * x1) / 100;
+                int x3 = n - 1000 * x1 - 100 * x2;
+                temp = Convert.ToInt32(Pow(x1, 4) + Pow(x2, 4)
+                    + Pow(x3 / 10, 4) + Pow(x3 % 10, 4));
+                if (temp == n)
+                {
+                    Console.WriteLine(n);
+                }
+            }
+        }
+    }
+}

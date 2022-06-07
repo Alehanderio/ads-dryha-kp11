6bd69bf4e6e0ac003b6387fa974c34ce6fbd8661
Alehanderio <72273009+Alehanderio@users.noreply.github.com>
2022-06-07 14:58:15 +0300

    just an upload

---
+++ heshTables/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace heshTables
+{
+    class Program
+    {
+        public static void allCommands()
+        {
+            Console.WriteLine("/help - list of commands;");
+            Console.WriteLine("/clr - clear console;");
+            Console.WriteLine("/q - exit.");
+        }
+
+        static void Main(string[] args)
+        {
+            allCommands();
+            while (true)
+            {
+                try
+                {
+                    string enterCommand = Console.ReadLine();
+                    switch (enterCommand)
+                    {
+                        case "/help":
+                            allCommands();
+                            break;
+                        case ("/q"):
+                            System.Environment.Exit(1);
+                            break;
+                        case "/clr":
+                            Console.Clear();
+                            allCommands();
+                            break;
+                        default:
+                            Console.WriteLine("Incorect command.");
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Unknown error!!");
+                }
+            }
+        }
+    }
+
+    class HashTable
+    {
+
+    }
+}
